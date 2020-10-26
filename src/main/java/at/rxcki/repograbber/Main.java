1d1356d0219791ae7930ea8fbcd510f86b9c77cf
Rxcki <patrick.zdarsky@gmail.com>
2020-10-26 10:29:44 +0100

    Add config file

--- src/main/java/at/rxcki/repograbber/Main.java
+++ src/main/java/at/rxcki/repograbber/Main.java
@@ -1,5 +1,6 @@
 package at.rxcki.repograbber;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,30 +8,38 @@ import org.bson.Document;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
 import java.util.List;
+import java.util.Properties;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class.getClassLoader());
 
-    static File mainFile = new File("Projects");
+    private static final File PROJECTS = new File("Projects");
+    private static Properties properties;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        mainFile.mkdir();
+        loadProperties();
+        if (properties == null)
+            return; //Can't start without the config
 
-        String projectsRaw = get("https://bitbucket.icevizion.de/rest/api/1.0/projects");
+        PROJECTS.mkdir();
+
+        String projectsRaw = get(properties.get("url")+"/rest/api/1.0/projects");
         LOGGER.info(projectsRaw);
 
         Document projects = Document.parse(projectsRaw);
         for (Document project : ((List<Document>) projects.get("values"))) {
             LOGGER.info(project.get("key"));
-            File projectFile = new File(mainFile.getAbsolutePath() + "/" + project.get("name"));
+            File projectFile = new File(PROJECTS.getAbsolutePath() + "/" + project.get("name"));
             projectFile.mkdir();
 
-            String projDetailRaw = get("https://DOMAIN/rest/api/1.0/projects/" + project.get("key") + "/repos");
+            String projDetailRaw = get(properties.get("url")+"/rest/api/1.0/projects/" + project.get("key") + "/repos");
             Document repositories = Document.parse(projDetailRaw);
             for (Document repo : ((List<Document>) repositories.get("values"))) {
                 File repoFile = new File(projectFile.getAbsolutePath() + "/" + repo.get("name"));
@@ -44,7 +53,7 @@ public class Main {
                 builder.inheritIO();
                 builder.start().waitFor();
 
-                String branchesRaw = get("https://DOMAIN/rest/api/1.0/projects/"+project.get("key")+"/repos/"+repo.getString("slug")+"/branches");
+                String branchesRaw = get(properties.get("url")+"/rest/api/1.0/projects/"+project.get("key")+"/repos/"+repo.getString("slug")+"/branches");
                 Document branches = Document.parse(branchesRaw);
                 for (Document branch : ((List<Document>) branches.get("values"))) {
                     builder = new ProcessBuilder("git", "checkout", branch.getString("displayId"));
@@ -56,14 +65,30 @@ public class Main {
         }
     }
 
+    private static void loadProperties() throws IOException {
+        File propertyFile = new File("Config.properties");
+        if (!propertyFile.exists()) {
+            LOGGER.warn("No config.properties found! Loading defaults and stopping...");
+            FileUtils.copyInputStreamToFile(Main.class.getClassLoader().getResourceAsStream("config.properties"), propertyFile);
+            return;
+        }
+
+        try (FileInputStream in = new FileInputStream(propertyFile)) {
+            properties = new Properties();
+            properties.load(in);
+        }
+        LOGGER.info("Loaded config!");
+    }
+
     public static String get(String url) throws IOException {
         URL url1 = new URL(url);
+        String authString = properties.get("username")+":"+properties.get("password");
 
-        HttpsURLConnection connection = (HttpsURLConnection) url1.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
         connection.setRequestMethod("GET");
         connection.addRequestProperty("Authorization",
                 "Basic " + Base64.getEncoder().encodeToString(
-                        "username:password".getBytes()));
+                        authString.getBytes()));
 
         connection.connect();
         StringWriter writer = new StringWriter();
